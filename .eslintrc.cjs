de057807852a4ec0993a874fa74be39401f1d802
callmeyan <yaclty2@gmail.com>
2025-01-15 22:11:08 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    '@typescript-eslint/no-explicit-any':[
+        'off'
+    ],
+    'no-mixed-spaces-and-tabs':'off',
+    'no-control-regex':0
+  },
+}
