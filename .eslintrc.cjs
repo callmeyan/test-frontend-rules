118620642c6a1177df1a93f5dc7cb8f09d8da21b
callmeyan <yaclty2@gmail.com>
2025-01-16 11:09:28 +0800

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    '@typescript-eslint/no-explicit-any':[
-        'off'
+    root: true,
+    env: {browser: true, es2020: true},
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        // "plugin:vue/vue3-recommended",
     ],
-    'no-mixed-spaces-and-tabs':'off',
-    'no-control-regex':0
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-explicit-any': [
+            'off'
+        ],
+        'vue/max-attributes-per-line':'off',
+        'vue/html-self-closing':'off',
+        'no-mixed-spaces-and-tabs': 'off',
+        'no-control-regex': 0
+    },
 }
